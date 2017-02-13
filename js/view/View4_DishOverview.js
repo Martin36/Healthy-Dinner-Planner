4438c93430a4d90e2f0260c7ec8777ce3754e684
Staffan Sandberg <Staffan.Sandberg94@gmail.com>
2017-02-13 13:42:47 +0100

    Dish overview has update

--- js/view/View4_DishOverview.js
+++ js/view/View4_DishOverview.js
@@ -1,40 +1,42 @@
 //ExampleView Object constructor
 var View4_DishOverview = function(container, model) {
 
+  // Add the view to the controller
+  model.addObserver(this);
   // Get all the relevant elements of the view (ones that show data
   // and/or ones that responed to interaction)
-  var dishContainer = container.find("#selectedDishes");
   var dishImage = container.find("img");
   var dishTitle = container.find("#dishTitle");
   var prepIns = container.find("#preparations p");
   var listTitle = container.find("#listTitle");
-  var ingredients = container.find("#ingredientList");
   var totalCost = container.find("#totalCost");
 
-  var selectedDish = model.getDish(1);
-  var nr = model.getNumberOfGuests();
+  var updateDishToShow = function() {
+    var selectedDish = model.getDish(1); // This should be the dish you clicked on
+    var nr = model.getNumberOfGuests();
 
-  dishImage.attr("src", "images/" + selectedDish.image);
-  dishTitle.html(selectedDish.name);
-  prepIns.html(selectedDish.description);
-  listTitle.html("Ingredients for " + nr + " People");
+    dishImage.attr("src", "images/" + selectedDish.image);
+    dishTitle.html(selectedDish.name);
+    prepIns.html(selectedDish.description);
+    listTitle.html("Ingredients for " + nr + " People");
 
-  var totalPrice = 0;
-  for (var i = 0; i < selectedDish.ingredients.length; i++) {
-    var ingredient = selectedDish.ingredients[i];
-    var amount = $("<div>").addClass("col-md-3 space");
-    amount.html((parseFloat(ingredient.quantity) * nr).toString() + " " + ingredient.unit);
-    var name = $("<div>").addClass("col-md-5 space").html(ingredient.name);
-    var priceTemp = parseFloat(ingredient.price) * nr;
-    var price = $("<div>").addClass("col-md-4 space");
-    var priceText = $("<p>").addClass("alignRight").html("SEK " + priceTemp)
-
-    ingredients.append(amount);
-    ingredients.append(name);
-    ingredients.append(price);
-    price.append(priceText);
-    totalPrice += priceTemp;
+    // Go through all the ingredients
+    var totalPrice = 0;
+    for (var i = 0; i < selectedDish.ingredients.length; i++) {
+      var ingredient = selectedDish.ingredients[i];
+      var piecePrice = parseFloat(ingredient.price) * nr;
+      container.find("#ingredientList")
+        .append($("<div>").addClass("col-md-3 space")
+          .html((parseFloat(ingredient.quantity) * nr).toString() + " " + ingredient.unit))
+        .append($("<div>").addClass("col-md-5 space").html(ingredient.name))
+        .append($("<div>").addClass("col-md-4 space").append($("<p>").addClass("alignRight").html("SEK " + piecePrice)));
+      totalPrice += piecePrice;
+    }
+    totalCost.html("SEK " + totalPrice.toString());
   }
 
-  totalCost.html("SEK " + totalPrice.toString());
+  // Maybe only update ingredient amount and cost if only nr of guests are changed.
+  this.update = function(obj) {
+    updateDishToShow();
+  }
 }
