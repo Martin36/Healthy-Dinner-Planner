1edaf7d4b8bf4d742783861fa6d8f6a4c4bab43e
Staffan Sandberg <Staffan.Sandberg94@gmail.com>
2017-02-13 14:20:12 +0100

    Selected dishes update started

--- js/view/View2_SelectedDishesView.js
+++ js/view/View2_SelectedDishesView.js
@@ -1,32 +1,35 @@
 //ExampleView Object constructor
 var View2_SelectedDishesView = function(container, model) {
 
+  model.addDishToMenu(2);
+
+  model.addObserver(this);
+
   // Get all the relevant elements of the view (ones that show data
   // and/or ones that responed to interaction)
   var dishContainer = container.find("#selectedDishes");
   var selectedDishes = model.getFullMenu();
   var totalPrice = container.find("#totalPrice");
 
-  //console.log(this.dishContainer);
+  var refreshSelectedDishes = function() {
+    totalPrice.html("SEK " + model.getTotalMenuPrice().toString());
 
-  totalPrice.html("SEK " + model.getTotalMenuPrice().toString());
+    for (var i = 0; i < selectedDishes.length; i++) {
+      var dish = selectedDishes[i];
+      dishContainer
+        .append($("<div>").addClass("row")
+          .append($("<div>").addClass("col-md-12")
+            .append($("<div>").addClass("well margin")
+              .append($("<p>").addClass("alignLeft").html(dish.name))
+              .append($("<p>").addClass("alignRight").html(model.getDishPrice(dish.id)))
+              .append($("<button>").addClass("btn btn-danger circle")
+                .append($("<span>").addClass("glyphicon glyphicon-remove"))))));
+    }
+  }
 
-  for (var i = 0; i < selectedDishes.length; i++) {
-    var dish = selectedDishes[i];
-    var selDish = $("<div>").addClass("row");
-    var col = $("<div>").addClass("col-md-12");
-    var well = $("<div>").addClass("well margin");
-    var title = $("<p>").addClass("alignLeft").html(dish.name);
-    var price = $("<p>").addClass("alignRight").html(model.getDishPrice(dish.id));
-    var button = $("<button>").addClass("btn btn-danger circle");
-    var cross = $("<span>").addClass("glyphicon glyphicon-remove");
+  var clearSelectedDishes = function() {}
 
-    selDish.append(col);
-    col.append(well);
-    well.append(title);
-    well.append(price);
-    well.append(button);
-    button.append(cross);
-    dishContainer.append(selDish);
+  this.update = function() {
+
   }
 }
