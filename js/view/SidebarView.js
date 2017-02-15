46bbbd929fb77ab3ddd167fa78dd28481bde4c2b
Staffan Sandberg <Staffan.Sandberg94@gmail.com>
2017-02-15 14:41:17 +0100

    removing dishes toggle container in sidebar.

--- js/view/SidebarView.js
+++ js/view/SidebarView.js
@@ -10,14 +10,49 @@ var SidebarView = function(container, model) {
   var dishContainer = container.find("#selectedDishes");
   var selectedDishes = model.getFullMenu();
   var totalPrice = container.find("#totalPrice");
+  var dishTypes = ["starter", "main course", "dessert"];
+
+  var initSelectedDishes = function(){
+    var dishID = ""
+    for (dish in dishTypes){
+      if(dishTypes[dish] == "main course")
+        dishID = "maincourse";
+      else
+        dishID = dishTypes[dish];
+
+      dishContainer
+      .append($("<div>").addClass("row").attr("id", dishID+"Container")
+        .append($("<div>").addClass("col-md-12")
+          .append($("<div>").addClass("well margin")
+            .append($("<p>").addClass("alignLeft").html(dishTypes[dish]))
+            .append($("<p>").addClass("alignRight"))//.html(model.getDishPrice(dish.id)))
+            .append($("<button>").addClass("btn btn-danger circle")
+            .attr("type", "button").attr("id", "delete"+dishID+"Button")
+              .append($("<span>").addClass("glyphicon glyphicon-remove"))))));
+    }
+  }
+
+  initSelectedDishes();
+  this.deleteStarterButton = container.find("#deletestarterButton");
+  this.deleteMainCourseButton = container.find("#deletemaincourseButton");
+  this.deleteDessertButton = container.find("#deletedessertButton");
+
+  this.starterContainer = container.find("#starterContainer");
+  this.mainCourseContainer = container.find("#maincourseContainer");
+  this.dessertContainer = container.find("#dessertContainer");
 
   var addSelectedDishes = function() {
     totalPrice.html("SEK " + model.getTotalMenuPrice().toString());
-
-    for (var i = 0; i < selectedDishes.length; i++) {
+    /*
+    // Can only choose 3
+    for (var i = 0; i < 3; i++) {
       var dish = selectedDishes[i];
+      var dishExists = "display:null";
+      if(dish == undefined) {
+        dishExists = "display:none";
+      }
       dishContainer
-        .append($("<div>").addClass("row")
+        .append($("<div>").addClass("row").attr("style", dishExists)
           .append($("<div>").addClass("col-md-12")
             .append($("<div>").addClass("well margin")
               .append($("<p>").addClass("alignLeft").html(dish.name))
@@ -26,10 +61,13 @@ var SidebarView = function(container, model) {
               .attr("type", "button").attr("dishid", dish.id)
                 .append($("<span>").addClass("glyphicon glyphicon-remove"))))));
     }
+    */
   }
 
-  addSelectedDishes();
-  this.deleteButton = container.find("#deleteDish");
+
+  //addSelectedDishes();
+
+  //this.deleteButton = container.find("#deleteDish");
   var clearSelectedDishes = function() {
     while (dishContainer.firstChild) {
         dishContainer.removeChild(dishContainer.firstChild);
