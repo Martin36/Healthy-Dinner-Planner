ef40812e91715218ffbc1e826ab1d7e07ae13da0
Sandsten <Staffan.Sandberg94@gmail.com>
2017-02-28 18:14:45 +0100

    Search circle fixed

--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -5,11 +5,15 @@ dinnerPlannerApp.controller('SearchCtrl', function ($scope,Dinner) {
   // TODO in Lab 5: you will need to implement a method that searchers for dishes
   // including the case while the search is still running.
 
+  $scope.showSearch = false;
+
   $scope.search = function(query,type) {
-   $scope.status = "Searching...";
+   //$scope.status = "Searching...";
+   $scope.showSearch = true;
    Dinner.DishSearch.get({query:query,type:type},function(data){
      $scope.dishes=data.results;
-     $scope.status = "Showing " + data.results.length + " results";
+     //$scope.status = "Showing " + data.results.length + " results";
+     $scope.showSearch = false;
    },function(data){
      $scope.status = "There was an error";
    });
