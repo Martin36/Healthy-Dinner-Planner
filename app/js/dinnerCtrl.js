dd5e2640516435ef7f84e37a57c0c6f4da79a49d
Sandsten <Staffan.Sandberg94@gmail.com>
2017-02-28 15:09:49 +0100

    View 3 and 2 stuff

--- app/js/dinnerCtrl.js
+++ app/js/dinnerCtrl.js
@@ -1,4 +1,4 @@
-// Dinner controller that we use whenever we have view that needs to 
+// Dinner controller that we use whenever we have view that needs to
 // display or modify the dinner menu
 dinnerPlannerApp.controller('DinnerCtrl', function ($scope,Dinner) {
 
@@ -11,6 +11,13 @@ dinnerPlannerApp.controller('DinnerCtrl', function ($scope,Dinner) {
   $scope.getNumberOfGuests = function() {
     return Dinner.getNumberOfGuests();
   }
+
+  $scope.incrementGuests = function() {
+    console.log("Hej");
+    var guests = Dinner.getNumberOfGuests() + 1;
+    Dinner.setNumberOfGuests(guests);
+  }
+
 
   // TODO in Lab 5: Implement the methods to get the dinner menu
   // add dish to menu and get total menu price
