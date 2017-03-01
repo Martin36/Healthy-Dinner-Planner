755540aec1d4180b850d48d06e953b84f4048fb6
Sandsten <Staffan.Sandberg94@gmail.com>
2017-03-01 12:43:45 +0100

    Debug stuff cleanup.

--- app/js/dishCtrl.js
+++ app/js/dishCtrl.js
@@ -5,20 +5,10 @@ dinnerPlannerApp.controller('DishCtrl', function ($scope,$routeParams,Dinner) {
   // TODO in Lab 5: you need to get the dish according to the routing parameter
   // $routingParams.paramName
   // Check the app.js to figure out what is the paramName in this case
-
-  $scope.nrOfGuests = Dinner.getNumberOfGuests();
+  $scope.selectedDish = Dinner.Dish.get({id:$routeParams.dishId});
 
   $scope.getNumberOfGuests = function(){
     return Dinner.getNumberOfGuests();
-  }
-  console.log($scope.nrOfGuests);
-
-  $scope.selectedDish = Dinner.Dish.get({id:$routeParams.dishId});
-  console.log($scope.selectedDish);
-
-  //Amount for single ingredient
-  $scope.piecePrice = function(amount){
-    return (parseFloat(amount) * Dinner.getNumberOfGuests()).toFixed(1);
   }
 
 });
